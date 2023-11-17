36548e113dd876bf3eedc0bc4ad730f9d620298a
Romain HOCQUET <romain.hocquet@etu.univ-grenoble-alpes.fr>
2023-11-17 11:11:25 +0100

    solution direct incomplet

--- src/prodcons/v1/ProdConsBuffer.java
+++ src/prodcons/v1/ProdConsBuffer.java
@@ -1,5 +1,44 @@
 package prodcons.v1;
 
-public class ProdConsBuffer {
+public class ProdConsBuffer implements IProdConsBuffer{
 
+	Message[] mBuffer;
+	int nMessage; //number message
+	
+	public ProdConsBuffer(int bufferSize) {
+		this.mBuffer = new Message[bufferSize];
+	}
+	
+	private boolean notFull() {
+		return nmsg() < this.mBuffer.length;
+	}
+	private boolean notEmpty() {
+		return nmsg() > 0;
+	}
+	@Override
+	//Produce
+	public void put(Message m) throws InterruptedException { 
+		
+	}
+
+	@Override
+	//Consume
+	public Message get() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int nmsg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int totmsg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 }
